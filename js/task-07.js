ffd9fa6a528083bda80702c2d8b2adaa17f74197
Tanya-Meleshko <melehhha@gmail.com>
2022-09-11 19:53:07 +0300

    Formats task 7

--- js/task-07.js
+++ js/task-07.js
@@ -5,10 +5,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
-const onInputChange = (event) => {
-
-    textEl.style.fontSize = `${event.currentTarget.value}px`
+const onInputChange = event => {
+  textEl.style.fontSize = `${event.currentTarget.value}px`;
 };
 
 inputEl.addEventListener('input', onInputChange);
-
