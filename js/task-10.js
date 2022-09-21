68669e77b79c73b580fae6afa806a99412cfdb2f
Tanya-Meleshko <melehhha@gmail.com>
2022-09-21 18:58:34 +0300

    Solves task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,35 +1,53 @@
-// Напиши скрипт создания и очистки коллекции элементов. 
+// Напиши скрипт создания и очистки коллекции элементов.
 // Пользователь вводит количество элементов в input и нажимает кнопку Создать,
 //   после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
 
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. 
+// Создай функцию createBoxes(amount), которая принимает один параметр - число.
 // Функция создает столько < div >, сколько указано в amount и добавляет их в div#boxes.
 
 // Размеры самого первого <div> - 30px на 30px.
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. 
+// Все элементы должены иметь случайный цвет фона в формате HEX.
 // Используй готовую функцию getRandomHexColor для получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
 
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes,
 //   тем самым удаляя все созданные элементы.
 
 const refs = {
-  boxes: document.querySelector('#boxes'),
-  buttonCreate: document.querySelector('#[data-create]'),
-  buttonCreate: document.querySelector('#[data-destroy]'),
+  inputAmount: document.querySelector('input'),
+  boxWrapper: document.querySelector('#boxes'),
+  buttonCreate: document.querySelector('[data-create]'),
+  buttonDestroy: document.querySelector('[data-destroy]'),
 };
 
-function createBoxes(amount) {
-  
-};
+refs.buttonDestroy.addEventListener('click', destroyBoxes);
 
-
+refs.buttonCreate.addEventListener('click', () => {
+  createBoxes(Number(refs.inputAmount.value));
+});
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function destroyBoxes() {
+  refs.boxWrapper.innerHTML = '';
+  refs.inputAmount.value = '';
+}
+
+function createBoxes(amount) {
+  const box = '<div></div>';
+  const boxes = Array(amount).fill(box).join('');
+  refs.boxWrapper.insertAdjacentHTML('afterbegin', boxes);
+  const divs = [...refs.boxWrapper.children];
+  let size = 30;
+  const step = 10;
+  divs.map(div => {
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    size += step;
+  });
+}
